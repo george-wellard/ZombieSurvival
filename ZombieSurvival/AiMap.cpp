deaa9a20f56cfc4f98630dc301b85063b8e395d5
George Wellard <georgewellard03@gmail.com>
2021-03-19 22:11:32 +0000

    Map for AI set

--- ZombieSurvival/AiMap.cpp
+++ ZombieSurvival/AiMap.cpp
@@ -1,1 +1,28 @@
-#include "AiMap.h"
+#include"AiMap.h"
+
+AiMap::AiMap()
+{
+	// Paramters for the map 
+	float width = 17.5;
+	float height = 17.275;
+	float xBuffer = 15;
+	float yBuffer = 13;
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			mapArray[i][j] = new Node(xBuffer + width * j, yBuffer + height * i, width, height, i, j); // Each point of the array represents a node 
+		}
+	}
+}
+
+void AiMap::Draw(sf::RenderWindow* window)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			mapArray[i][j]->drawNode(*window);
+		}
+	}
+}
--- ZombieSurvival/AiMap.h
+++ ZombieSurvival/AiMap.h
@@ -1,5 +1,25 @@
 #pragma once
+#include "SFML\Graphics.hpp"
+#include "Node.h"
+#include <list>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
 class AiMap
 {
-};
+public:
 
+	AiMap();
+
+	void Draw(sf::RenderWindow* window);
+
+	static const short unsigned rows = 33;
+	static const short unsigned columns = 41;
+
+	Node* mapArray[rows][columns];
+
+private:
+
+};
