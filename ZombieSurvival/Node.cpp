e6c39e6e9dcdc30431793989a50327733c5af0c8
George Wellard <georgewellard03@gmail.com>
2021-03-21 19:40:02 +0000

    Set up map nodes

--- ZombieSurvival/Node.cpp
+++ ZombieSurvival/Node.cpp
@@ -1,1 +1,168 @@
 #include "Node.h"
+
+Node::Node()
+{
+}
+
+Node::Node(float xPosIn, float yPosIn, float heightIn, float widthIn, int rowIn, int columnIn)
+{
+	xPos = xPosIn;
+	yPos = yPosIn;
+	row = rowIn;
+	column = columnIn;
+	height = heightIn;
+	width = widthIn;
+	current = false;
+	diagonal = false;
+	canMove = true;
+
+	node.setSize(sf::Vector2f(width, height));
+	node.setPosition(sf::Vector2f(xPos, yPos));
+	node.setFillColor(sf::Color(255, 255, 255, 50));
+	node.setOutlineThickness(1);
+	node.setOutlineColor(sf::Color(255, 255, 255, 100));
+}
+
+void Node::drawNode(sf::RenderWindow& window)
+{
+	window.draw(node);
+}
+
+void Node::setWall()
+{
+	canMove = false;
+	node.setFillColor(sf::Color(100, 255, 255, 50));
+}
+
+void Node::resetWall()
+{
+	canMove = true;
+	node.setFillColor(sf::Color(100, 150, 150, 50));
+}
+
+void Node::setPath()
+{
+	node.setFillColor(sf::Color(255, 255, 255, 50));
+}
+
+void Node::setOpen()
+{
+	node.setFillColor(sf::Color(200, 100, 100, 50));
+}
+
+void Node::setClosed()
+{
+	node.setFillColor(sf::Color(100, 150, 150, 50));
+}
+
+void Node::setRow(int row)
+{
+	this->row = row;
+}
+
+void Node::setColumn(int column)
+{
+	this->column = column;
+}
+
+void Node::setGScore(float value)
+{
+	this->g = value;
+}
+
+void Node::setDiagonal(bool diagonal)
+{
+	this->diagonal = diagonal;
+}
+
+float Node::getFScore() const
+{
+	return f;
+}
+
+float Node::getGScore() const
+{
+	return g;
+}
+
+float Node::getHScore() const
+{
+	return h;
+}
+
+int Node::getX() const
+{
+	return xPos + width / 2;
+}
+
+int Node::getY() const
+{
+	return yPos + height / 2;
+}
+
+int Node::getRow() const
+{
+	return row;
+}
+
+int Node::getColumn() const
+{
+	return column;
+}
+
+bool Node::checkDiagonal()
+{
+	return diagonal;
+}
+
+bool Node::checkMoveable()
+{
+	return canMove;
+}
+
+bool Node::equals(Node& newNode)
+{
+	return (row == newNode.getRow() && column == newNode.getColumn());
+}
+
+void Node::fCalculation()
+{
+	f = g + h;
+}
+
+void Node::hCalculation(Node& goalNode)
+{
+	int goalX = goalNode.getX();
+	int goalY = goalNode.getY();
+	float dx, dy;
+
+	dx = (float)(goalX - xPos);
+	dy = (float)(goalY - yPos);
+	h = sqrt(dx * dx + dy * dy); // Getting the squre root to return a single value 
+}
+
+void Node::gCalculation(float parentGScore)
+{
+	if (firstGCal)
+	{
+		// 1.414 is for diagonal nodes
+		diagonal ? g = parentGScore + 1.414f : g = parentGScore + 1.0f;
+
+		diagonal ? new_g = parentGScore + 1.414f : new_g = parentGScore + 1.0f;
+		firstGCal = false;
+	}
+	else
+	{
+		diagonal ? new_g = parentGScore + 1.414f : new_g = parentGScore + 1.0f;
+	}
+}
+
+bool Node::canImproveG()
+{
+	return g >= new_g;
+}
+
+void Node::improveG()
+{
+	this->g = new_g;
+}
--- ZombieSurvival/Node.h
+++ ZombieSurvival/Node.h
@@ -1,5 +1,88 @@
 #pragma once
+#include "SFML\Graphics.hpp"
+
 class Node
 {
-};
+public:
 
+	Node();
+
+	Node(float xPosIn, float yPosIn, float heightIn, float widthIn, int rowIn, int columnIn);
+
+	void drawNode(sf::RenderWindow& window);
+
+	bool current = true;
+
+	// drawing
+	void setWall();
+
+	void resetWall();
+
+	void setPath();
+
+	void setOpen();
+
+	void setClosed();
+
+	// setting
+
+	void setRow(int row);
+
+	void setColumn(int column);
+
+	void setGScore(float value);
+
+	void setDiagonal(bool diagonal);
+
+	//getting
+	float getFScore() const;
+
+	float getGScore() const;
+
+	float getHScore() const;
+
+	int getX() const;
+
+	int getY() const;
+
+	int getRow() const;
+
+	int getColumn() const;
+
+	bool checkDiagonal();
+
+	bool checkMoveable();
+
+	// Calculations for path finding
+	bool equals(Node& newNode);
+
+	void fCalculation();
+
+	void hCalculation(Node& goalNode);
+
+	void gCalculation(float parentGScore);
+
+	bool canImproveG();
+
+	void improveG();
+
+private:
+
+	int xPos;
+	int yPos;
+	int width;
+	int height;
+	int row;
+	int column;
+
+	float f;
+	float g;
+	float h;
+	float new_g;
+
+	bool firstGCal;
+	bool diagonal;
+	bool canMove;
+
+	sf::RectangleShape node;
+};
